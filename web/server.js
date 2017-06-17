d154dd57ba28dbbf35b6f012ecbe1fb32354793f
substack <substack@9nTgtYmvW4HID6ayt6Icwc8WZxdifx5SlSKKIX/X/1g=.ed25519>
2017-06-17 19:22:14 +0200

    using ecstatic

--- web/server.js
+++ web/server.js
@@ -1,12 +1,12 @@
 var http = require('http')
+var fs = require('fs')
+var ecstatic = require('ecstatic')('public')
+
 var server = http.createServer(function (req, res) {
   console.log(req.method, req.url)
   if (req.method === 'GET' && req.url === '/') {
     res.setHeader('content-type', 'text/html')
     res.end('<h1>hello</h1>')
-  } else {
-    res.statusCode = 404
-    res.end('not found')
-  }
+  } else ecstatic(req, res)
 })
 server.listen(4000)
