592919449b17383435bd6c51d855e3f6c80f95d7
substack <substack@9nTgtYmvW4HID6ayt6Icwc8WZxdifx5SlSKKIX/X/1g=.ed25519>
2017-06-17 18:57:50 +0200

    chat server

---
+++ server/chatserver.js
@@ -0,0 +1,20 @@
+var net = require('net')
+var onend = require('end-of-stream')
+var split = require('split2')
+var to = require('to2')
+
+var streams = []
+var server = net.createServer(function (stream) {
+  streams.push(stream)
+  onend(stream, function () {
+    var ix = streams.indexOf(stream)
+    streams.splice(ix,1)
+  })
+  stream.pipe(split()).pipe(to(function (buf, enc, next) {
+    streams.forEach(function (s) {
+      s.write(buf + '\n')
+    })
+    next()
+  }))
+})
+server.listen(5000)
--- server/tcpclient.js
+++ server/tcpclient.js
@@ -1,5 +1,6 @@
 var net = require('net')
-var stream = net.connect(5000)
+var host = process.argv[2]
+var stream = net.connect(5000, host)
 process.stdin
   .pipe(stream)
   .pipe(process.stdout)
